066ebbb64cf76a7ee6f98953a648418ba4054d64
Xavier8L <shenglongchen99@gmail.com>
2024-01-08 12:04:12 +0100

    userauth console

--- src/Components/Pages/Bedrijf/BedrijfProfielPagina.js
+++ src/Components/Pages/Bedrijf/BedrijfProfielPagina.js
@@ -1,7 +1,7 @@
 import useAuth from "../../../hooks/useAuth";
 function BedrijfProfielPagina() {
     const { userAuth } = useAuth();
-    console.log(useAuth);
+    console.log(userAuth);
     return (
         <>
             <h1>BedrijfProfielPagina</h1>

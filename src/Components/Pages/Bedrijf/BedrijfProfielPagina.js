7be61a3d366be9ca85eb2366540abe26d026c2b9
tinepangsiu <tinepangsiu@gmail.com>
2024-01-10 20:58:57 +0100

    bedrijf preofiel

--- src/Components/Pages/Bedrijf/BedrijfProfielPagina.js
+++ src/Components/Pages/Bedrijf/BedrijfProfielPagina.js
@@ -1,12 +1,59 @@
-import useAuth from "../../../hooks/useAuth";
+import 'bootstrap/dist/css/bootstrap.css';
+import '../../../css/styles.css';
+import '../../../css/BedrijfProfielPagina.css';
+import { useRef, useState, useEffect } from 'react';
+import useAuth from '../../../hooks/useAuth';
+import axios from '../../../api/axios';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+
 function BedrijfProfielPagina() {
+
     const { userAuth } = useAuth();
-    console.log(useAuth);
+    const [bedrijfsnaam, setBedrijfsnaam] = useState("");
+    const [email, setEmail] = useState("");
+    const [locatie , setLocatie] = useState("");
+    const [wachtwoord, setWachtwoord] = useState("");
+    const [beschrijving, setBeschrijving] = useState("");
+    const [telefoon, setTelefoon] = useState("");
+    const [link, setLink] = useState("");
+
+
+
     return (
-        <>
-            <h1>BedrijfProfielPagina</h1>
-            
-        </>
+        <div class="row">
+            <div class="panel">
+                <div class="profiel-info">
+                    <h1>Bedrijfsprofiel</h1>
+                    <div class="row">
+                        <div class="bio-row">
+                            <p><span>Bedrijfsnaam </span>: {bedrijfsnaam}</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>E-mail </span>: {email}</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Locatie </span>:{locatie}</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Wachtwoord</span>: {wachtwoord}</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Beschrijving </span>: {beschrijving}</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Telefoon </span>: {telefoon}</p>
+                        </div>
+                        <div class="bio-row">
+                            <p><span>Website </span>: {link}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Wijzigen</button>
+                <button type="reset" class="btn btn-default">Annuleren</button>
+            </div>
+        </div>
     );
 }
 

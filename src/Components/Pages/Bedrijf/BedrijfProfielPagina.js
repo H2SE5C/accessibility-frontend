44bc19e955fe9df766761758ec8e171a3cd5d1db
tinepangsiu <tinepangsiu@gmail.com>
2024-01-11 13:50:39 +0100

    profiel update weegeven

--- src/Components/Pages/Bedrijf/BedrijfProfielPagina.js
+++ src/Components/Pages/Bedrijf/BedrijfProfielPagina.js
@@ -1,57 +1,65 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../../css/styles.css';
 import '../../../css/BedrijfProfielPagina.css';
-import { useRef, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useAuth from '../../../hooks/useAuth';
 import axios from '../../../api/axios';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 function BedrijfProfielPagina() {
+    const {userAuth} = useAuth();
+    const [bedrijf, setBedrijf] = useState({});
 
-    const { userAuth } = useAuth();
-    const [bedrijfsnaam, setBedrijfsnaam] = useState("");
-    const [email, setEmail] = useState("");
-    const [locatie , setLocatie] = useState("");
-    const [wachtwoord, setWachtwoord] = useState("");
-    const [beschrijving, setBeschrijving] = useState("");
-    const [telefoon, setTelefoon] = useState("");
-    const [link, setLink] = useState("");
+    const fetchBedrijf = async () => {
+        try {
+            const response = await axios.get(`/api/bedrijf/profiel`, {
+                headers: {
+                    'Authorization': `Bearer ${userAuth.token}`
+                }
+            });
+            setBedrijf(response.data);
+        } catch (error) {
+            console.error('Fout bij het ophalen van bedrijfsgegevens:', error);
+        }
+    }
 
-
+        
+    useEffect(() => {
+        fetchBedrijf();
+    }, []);
 
     return (
-        <div class="row">
-            <div class="panel">
-                <div class="profiel-info">
-                    <h1>Bedrijfsprofiel</h1>
-                    <div class="row">
-                        <div class="bio-row">
-                            <p><span>Bedrijfsnaam </span>: {bedrijfsnaam}</p>
-                        </div>
-                        <div class="bio-row">
-                            <p><span>E-mail </span>: {email}</p>
-                        </div>
-                        <div class="bio-row">
-                            <p><span>Locatie </span>:{locatie}</p>
-                        </div>
-                        <div class="bio-row">
-                            <p><span>Wachtwoord</span>: {wachtwoord}</p>
-                        </div>
-                        <div class="bio-row">
-                            <p><span>Beschrijving </span>: {beschrijving}</p>
-                        </div>
-                        <div class="bio-row">
-                            <p><span>Telefoon </span>: {telefoon}</p>
-                        </div>
-                        <div class="bio-row">
-                            <p><span>Website </span>: {link}</p>
+        <div className="row">
+            <div className="panel">
+                    <div className="profiel-info">
+                        <h1>Bedrijfsprofiel</h1>
+                        <div className="row">
+                            <div className="bio-row">
+                                <p><span>Bedrijfsnaam </span>: {bedrijf.bedrijfsnaam}</p>
+                            </div>
+                            <div className="bio-row">
+                                <p><span>E-mail </span>: {bedrijf.email}</p>
+                            </div>
+                            <div className="bio-row">
+                                <p><span>Locatie </span>: {bedrijf.locatie}</p>
+                            </div>
+                            <div className="bio-row">
+                                <p><span>Wachtwoord</span>: *******</p>
+                            </div>
+                            <div className="bio-row">
+                                <p><span>Beschrijving </span>: {bedrijf.omschrijving}</p>
+                            </div>
+                            <div className="bio-row">
+                                <p><span>Telefoon </span>: {bedrijf.phonenumber}</p>
+                            </div>
+                            <div className="bio-row">
+                                <p><span>Website </span>: {bedrijf.linkNaarBedrijf}</p>
+                            </div>
                         </div>
                     </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary">Wijzigen</button>
+                    <button type="reset" className="btn btn-default">Annuleren</button>
                 </div>
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary">Wijzigen</button>
-                <button type="reset" class="btn btn-default">Annuleren</button>
             </div>
         </div>
     );
